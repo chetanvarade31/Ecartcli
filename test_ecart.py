3922c4f24defd17b632672b5d043da8bb73de8f0
chetan varade <77948610+chetanvarade31@users.noreply.github.com>
2024-10-09 15:48:34 +0530

    Update test_ecart.py

--- test_ecart.py
+++ test_ecart.py
@@ -1,52 +1,43 @@
-from Ecart import add_category, add_tocart, newuser, showorder
-from database import addproduct, get_all_order, insert_category
-from model import Category, Product
+import unittest
+from Ecart import add_category, add_tocart, newuser
+from database import addproduct
+from model import Product
 
-def test_add_category():
-    category = 'Electronic'
-    expected_output = None
-    ouput = add_category(category=category)
-    assert ouput == expected_output
+class TestEcart(unittest.TestCase):
 
+    def test_add_category(self):
+        category = 'Electronic'
+        expected_output = None
+        output = add_category(category=category)
+        self.assertEqual(output, expected_output)
 
-def test_add_to_cart():
-    username, name, category_name, price, date = "chetan varade","Mobile", "electronic", "4560","23-5-2021"
-    expected_output = None
-    output = add_tocart(username= username, name= name, category_name= category_name, price= price, date= date)
-    assert output == expected_output
+    def test_add_to_cart(self):
+        username = "chetan varade"
+        name = "Mobile"
+        category_name = "electronic"
+        price = "4560"
+        date = "23-5-2021"
+        expected_output = None
+        output = add_tocart(username=username, name=name, category_name=category_name, price=price, date=date)
+        self.assertEqual(output, expected_output)
 
+    def test_new_user(self):
+        username = "chetanvarade"
+        password = "Chetan@31"
+        repassword = "Chetan@31"
+        expected_output = None
+        output = newuser(username=username, password=password, repassword=repassword)
+        self.assertEqual(output, expected_output)
 
-def test_new_user():
-    username, password, repassword = "chetanvarade","Chetan@31","Chetan@31"
-    expected_output = None
-    output = newuser(username=username, password=password, repassword= repassword)
-    assert output == expected_output
+    def test_product(self):
+        name = "Redmi 6A"
+        category = "Electronic"
+        price = "45620"
+        date = "23-02-2022"
+        product_obj = Product(id, name=name, category=category, price=price, date=date)
+        expected_output = None
+        output = addproduct(product_obj)
+        self.assertEqual(output, expected_output)
 
-
-def test_show_order():
-    expected_output = None
-    output = showorder(username=None)
-    assert output == expected_output
-
-
-def test_product():
-    name, category, price, date = "Redmi 6A", "Electronic", "45620","23-02-2022"
-    product_obj = Product(id,name=name, category= category, price= price, date= date)
-    expected_output = None
-    output = addproduct(product_obj)
-    assert output == expected_output
-
-
-def test_get_all_order():
-    expected_output = type(list())
-    output = get_all_order(username= None)
-    assert type(output) == expected_output
-
-
-def test_insert_category():
-    category = 'cloth'
-    category_obj = Category(category=category)
-    expected_output = None
-    output = insert_category(category_obj)
-    assert output == expected_output
-
+if __name__ == '__main__':
+    unittest.main()
